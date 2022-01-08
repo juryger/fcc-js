6177fe7ba31b9407a638fdc098b771df7d9a2767
Iurii Gerasimov <juryger@gmail.com>
2022-01-08 03:47:17 +0300

    rot13 cipher and phone validation algos

---
+++ js/phone-number-validation.js
@@ -0,0 +1,7 @@
+function telephoneCheck(str) {
+    let usPhoneRegex = /^(1\s?)?(\(\d{3}\)(?=\s?\d)|\d{3})[-\s]?(\d{3})[-\s]?(\d{4})$/;
+    return str.test(usPhoneRegex);
+  }
+  
+  let result = telephoneCheck("1 555-555-5555");
+  console.log(result);
